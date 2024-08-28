332157ba7bee9fcea7244e5a75d3b2bbb13a9b87
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-08-28 20:23:26 +0300

    collapsible change

--- easybulgaria/components/Collapsible.tsx
+++ easybulgaria/components/Collapsible.tsx
@@ -1,16 +1,17 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
 import { PropsWithChildren, useState } from 'react';
 import { StyleSheet, TouchableOpacity, useColorScheme } from 'react-native';
 
 import { Colors } from '@/constants/Colors';
+import { Text, View } from 'react-native-reanimated/lib/typescript/Animated';
 
 export function Collapsible({ children, title }: PropsWithChildren & { title: string }) {
   const [isOpen, setIsOpen] = useState(false);
   const theme = useColorScheme() ?? 'light';
 
   return (
-    <>
-    </>
+    <View>
+      <Text>Hello</Text>
+    </View>
   );
 }
 
